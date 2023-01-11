a5e1b4f730c509de5eed8fd03c1416354f8ac717
kunal chauhan <kckr0115@gmail.com>
2023-01-11 16:10:57 +0530

    first commit

---
+++ routes/blog.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router()
+
+router.get('/', (req, res)=>{
+ res.sendFile(path.join(__dirname, "../views/index.html"))
+   
+})
+
+router.get('/blog', (req, res)=>{
+
+   res.sendFile(path.join(__dirname,  "../views/blogpage.html"))
+})
+
+
+module.exports = router
